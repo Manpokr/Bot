fd6c55be6a9adfe66185129e9495e5eaeb27eb89
Manpokr <90512305+Manpokr@users.noreply.github.com>
2023-08-16 18:04:05 +0800

    Update bot-vpn.sh

--- bot-vpn.sh
+++ bot-vpn.sh
@@ -7547,12 +7547,8 @@ fi
 }
 
 limit=$5
-if [[ $limit -gt 0 ]]; then
-   echo -e "$[$limit * 1024 * 1024 * 1024]" > /etc/manternet/limit/vless/quota/$user
-   limit_nya=$(printf `echo $(cat /etc/manternet/limit/vless/quota/$user) | numfmt --to=iec-i --suffix=B --format="%.1f" | column -t`)
-else
-   limit_nya="Unlimited"
-fi
+echo -e "$[$limit * 1024 * 1024 * 1024]" > /etc/manternet/limit/vless/quota/$userna
+limit_nya=$(printf `echo $(cat /etc/manternet/limit/vless/quota/$userna) | numfmt --to=iec-i --suffix=B --format="%.1f" | column -t`)
 
 none="$(cat ~/log-install.txt | grep -w "XRAY VLESS WS NTLS" | cut -d: -f2|sed 's/ //g')"
 xtls="$(cat ~/log-install.txt | grep -w "XRAY VLESS WS TLS" | cut -d: -f2|sed 's/ //g')"
