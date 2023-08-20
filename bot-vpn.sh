fd5054762fb0e88f49cdc41ed7e4d4bca0b2cad2
Manpokr <90512305+Manpokr@users.noreply.github.com>
2023-08-21 03:06:23 +0800

    Update bot-vpn.sh

--- bot-vpn.sh
+++ bot-vpn.sh
@@ -893,9 +893,9 @@ if [[ "${callback_query_from_id[$id]}" == "$get_AdminID" ]]; then
 echo -n > /tmp/other.txt
 data=( `cat /usr/local/etc/xray/user.txt | grep 'VL' | cut -d ' ' -f 2 | sort | uniq`);
 echo -e "";
-echo -e "━━━━━━━━━━━━━━━━━━━━━" > /tmp/vless-login
-echo -e "         🟢 VLess User Login 🟢  " >> /tmp/vless-login
-echo -e "━━━━━━━━━━━━━━━━━━━━━" >> /tmp/vless-login
+echo -e "━━━━━━━━━━━━━━━━━━━━━" 
+echo -e "         🟢 VLess User Login 🟢 "
+echo -e "━━━━━━━━━━━━━━━━━━━━━" 
 
 for akun in "${data[@]}"
 do
