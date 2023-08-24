c8eb9b2cdcc4a97e806ce471f0882cd1cf81c605
Manpokr <90512305+Manpokr@users.noreply.github.com>
2023-08-25 03:24:19 +0800

    Update bot-vpn.sh

--- bot-vpn.sh
+++ bot-vpn.sh
@@ -3229,9 +3229,9 @@ while :; do
                         duration=30
 			exp=30
                     fi
-		    userna="Trial-$( </dev/urandom tr -dc 0-9A-Z | head -c4 )"
-		    echo "$userna $exp" >>$CAD_ARQ
-		    user=$(cut -d' ' -f1 $CAD_ARQ)
+		    user="Trial-$( </dev/urandom tr -dc 0-9A-Z | head -c4 )"
+		   # echo "$userna" >>$CAD_ARQ
+		   # user=$(cut -d' ' -f1 $CAD_ARQ)
 		    vouch=$(tr </dev/urandom -dc a-zA-Z0-9 | head -c8)
 		    if grep -E "^TR $user" /usr/local/etc/xray/user.txt; then
                         ShellBot.sendMessage --chat_id ${message_chat_id[$id]} \
