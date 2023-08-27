a540283f9355232b938fc3188bba5c1918ff0661
Manpokr <90512305+Manpokr@users.noreply.github.com>
2023-08-28 03:35:42 +0800

    Update bot-vpn.sh

--- bot-vpn.sh
+++ bot-vpn.sh
@@ -1615,6 +1615,7 @@ vless_kota() {
 }
 
 create_vless() {
+    ShellBot.deleteMessage --chat_id ${callback_query_message_chat_id[$id]} --message_id ${callback_query_message_message_id[$id]}
     file_user=$1
     user=$(grep 'start [^_]*' $file_user | grep -o '[^_]*' | cut -d' ' -f2 | sed -n '2p')
     coupon=$(grep 'start [^_]*' $file_user | grep -o '[^_]*' | cut -d' ' -f2 | sed -n '3p')
